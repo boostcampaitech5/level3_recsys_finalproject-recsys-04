07b461dd644e4036acd83032bc5b59a1a30101ab
zsmalla <malla7454@gmail.com>
2023-07-16 16:59:27 +0900

    feat : set bootstrap-vue-3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// bootstrap setting
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
+
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount("#app");
