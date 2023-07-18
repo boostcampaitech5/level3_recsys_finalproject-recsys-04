431ddfece78d2763451aed23939d2f0ed57dcae8
zsmalla <malla7454@gmail.com>
2023-07-18 21:12:59 +0900

    add vue form dependency & setting

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,14 +4,26 @@
     <div id="content" class="p-4 p-md-5 pt-5">
       <h2 class="mb-4">당신이 찾는 모든 커피</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </p>
       <b-container class="bv-example-row">
-        <b-row class = "pt-5" v-for="num in 10" :key="num">
-          <b-col v-for="n in 3" :key="n"> <Card/> </b-col>
+        <b-row class="pt-5" v-for="num in 10" :key="num">
+          <b-col v-for="n in 3" :key="n"> <Card /> </b-col>
         </b-row>
       </b-container>
     </div>
@@ -21,22 +33,48 @@
         <h5>Filter</h5>
         <ul class="list-unstyled components mb-5">
           <li>
-            <a>
-              신맛
-            </a>
-            <b-form-input id="range-1" v-model="value" type="range" min="1" max="5" class="scroll"></b-form-input>
+            <a> 신맛 </a>
+            <b-form-input
+              id="range-1"
+              v-model="value"
+              type="range"
+              min="1"
+              max="5"
+              class="scroll"
+            ></b-form-input>
           </li>
           <li>
             <a>단맛</a>
-            <b-form-input id="range-1" v-model="value" type="range" min="1" max="5" class="scroll"></b-form-input>
+            <b-form-input
+              id="range-1"
+              v-model="value"
+              type="range"
+              min="1"
+              max="5"
+              class="scroll"
+            ></b-form-input>
           </li>
           <li>
             <a>바디감</a>
-            <b-form-input id="range-1" v-model="value" type="range" min="1" max="5" class="scroll"></b-form-input>
+            <b-form-input
+              id="range-1"
+              v-model="value"
+              type="range"
+              min="1"
+              max="5"
+              class="scroll"
+            ></b-form-input>
           </li>
           <li>
             <a>로스팅</a>
-            <b-form-input id="range-1" v-model="value" type="range" min="1" max="5" class="scroll"></b-form-input>
+            <b-form-input
+              id="range-1"
+              v-model="value"
+              type="range"
+              min="1"
+              max="5"
+              class="scroll"
+            ></b-form-input>
           </li>
         </ul>
         <div class="mb-5">
@@ -58,27 +96,26 @@
             <a href="#" class="tag-cloud-link">desserts</a>
           </div>
         </div>
-
       </div>
     </nav>
   </div>
 </template>
 
 <script>
-import Card from './Card.vue';
+import Card from "./Card.vue";
 
 export default {
   name: "products-main",
-  components:{
+  components: {
     Card,
-  }
+  },
 };
 </script>
 
 <style scoped>
-@import './style.css';
+@import "./style.css";
 
-h5{
-  color:black
+h5 {
+  color: black;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 
+// vue form setting
+import "./assets/main.scss";
+import Vueform from "@vueform/vueform/plugin";
+import vueformConfig from "./../vueform.config";
+
+
 // bootstrap setting
 import BootstrapVue3 from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
 
+
 const app = createApp(App)
 app.use(BootstrapVue3)
 app.use(router)
+app.use(Vueform, vueformConfig);
 app.mount("#app");
