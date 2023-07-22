dfa801ec41bc58fcd70afc8229bb1bd4b7ae95ec
Woo-Yeol <king199276@gmail.com>
2023-07-22 14:19:23 +0900

    feat : add thumbnail column

--- reconi_backend/coffee_bean/models.py
+++ reconi_backend/coffee_bean/models.py
@@ -8,6 +8,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
+def thumbnail_directory_path(instance):
+    return "thumbnails/{0}.png".format(instance.title)
+
+
 ReconiUser = get_user_model()
 
 
@@ -34,6 +39,9 @@ class CoffeeBean(models.Model):
     roastery = models.CharField(max_length=20, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     coupang_link = models.TextField(blank=True, null=True)
+    thumbnail = models.ImageField(
+        upload_to=thumbnail_directory_path, null=True, blank=True
+    )
 
     def __str__(self):
         return str(self.title)
--- reconi_backend/reconi_backend/settings.py
+++ reconi_backend/reconi_backend/settings.py
@@ -305,6 +305,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+# STATICFILES_DIRS = [
+#     # os.path.join(BASE_DIR, "coffee_bean", "static"),
+#     # os.path.join(BASE_DIR, "user", "static"),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
