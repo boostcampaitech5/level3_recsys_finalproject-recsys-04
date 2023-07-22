b6ac3ea48c30d65717d8643e87ecb44588a6c83b
Woo-Yeol <king199276@gmail.com>
2023-07-22 18:27:46 +0900

    feat : add Media Path

--- reconi_backend/reconi_backend/urls.py
+++ reconi_backend/reconi_backend/urls.py
@@ -20,6 +20,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
     openapi.Info(
         title="API 문서",
@@ -52,3 +55,5 @@ urlpatterns = [
     # RESTful Framework
     path("api-auth/", include("rest_framework.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
